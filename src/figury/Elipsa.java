59e8e6f7f66b90838ca53a0b5bdab79732e6fbca
j-brela <33935829+j-brela@users.noreply.github.com>
2017-11-24 18:26:19 +0100

    Initial commit

---
+++ src/figury/Elipsa.java
@@ -0,0 +1,14 @@
+package figury;
+
+import java.awt.Graphics2D;
+
+public class Elipsa extends figura{
+
+	public Elipsa(Graphics2D buf, int del, int w, int h) {
+		super(buf, del, w, h);
+		
+		
+		
+	}
+
+}
---
+++ src/figury/Kwadrat.java
@@ -0,0 +1,17 @@
+package figury;
+
+import java.awt.Graphics2D;
+
+public class Kwadrat extends figura {
+
+	public Kwadrat(Graphics2D buf, int del, int w, int h) {
+		super(buf, del, w, h);
+		
+		
+	}
+
+	
+	
+	
+	
+}
